2e982d2689ce88e17f3976042f70d03736e31049
chenwenkang <cwk1998@hotmail.com>
2018-09-04 21:47:52 +0800

    added views, but require more consideration on code design

--- justasklah/api.py
+++ justasklah/api.py
@@ -1,24 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from flask.views import MethodView
+from .db import create_session
 from flask_socketio import emit, Namespace
 
 bp = Blueprint('', __name__, url_prefix='/')
 
+
 class RoomView(MethodView):
-    def get(self):
-        pass
-    
+
+    def get(self, room_uid):
+        session = create_session()
+        data = session.execute(
+            "select * from room where room_uid = ?", room_uid)
+        if len(data) <= 1:
+            return jsonify(data)
+
     def post(self):
         pass
+
 
 class UserView(MethodView):
     def get(self):
         pass
-    
+
     def post(self):
         pass
-        
+
+
 class ChatSocket(Namespace):
     pass
-
-
