85ae563ad0de103ed72a0cdfff093226acfe7988
chenwenkang <cwk1998@hotmail.com>
2018-09-07 01:06:07 +0800

    create models for the cassandra databse

--- justasklah/models.py
+++ justasklah/models.py
@@ -1,14 +1,29 @@
 from cassandra.cqlengine.models import Model, columns
+import uuid
+from datetime import datetime
+
 
 class User(Model):
-    room = columns.Text()
+    id = columns.UUID(primary_key=True, partition_key=True, default=uuid.uuid4)
+    session_hash = columns.Text()
+    name = columns.Text()
+    created_time = columns.DateTime(default=datetime.utcnow())
+
 
 class Room(Model):
-    uid = columns.UUID()
+    id = columns.UUID(primary_key=True, partition_key=True, default=uuid.uuid4)
+    room_key = columns.Text(max_length=10) # h.encode(int(time.time()*10))
+    title = columns.Text(max_length=50)
+    description = columns.Text()
+    password = columns.Text()
+    created_time = columns.DateTime(default=datetime.utcnow())
 
-class Chat(Model):
-    timestamp = columns.DateTime()
+
+class Message(Model):
+    id = columns.UUID(primary_key=True, partition_key=True, default=uuid.uuid4)
+    timestamp = columns.DateTime(index=True, default=datetime.utcnow())
     content = columns.Text()
-    user = columns.Text()
+    room = columns.UUID()
+    user = columns.UUID()
     likes = columns.Counter()
-    dislikes = columns.Counter()
+    dismissed = columns.Boolean()
