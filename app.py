713f6e19d9991a85eabdcdc69ce8177c4fe385c8
chenwenkang <cwk1998@hotmail.com>
2018-09-08 21:18:16 +0800

    pass config to app factory for ease of initialization

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from justasklah import create_app, create_socketio
 from config import CURRENT_CONFIG
-app = create_app()
-app.config.from_object(CURRENT_CONFIG)
+app = create_app(CURRENT_CONFIG)
 socketio = create_socketio(app)
 
 if __name__ == '__main__':
--- justasklah/__init__.py
+++ justasklah/__init__.py
@@ -6,8 +6,9 @@ from .db import __init_db
 from .api import api_bp, MessageSocket
 
 
-def create_app():
+def create_app(config):
     app = Flask(__name__)
+    app.config.from_object(config)
     app.register_blueprint(api_bp)
     __init_db(app)
     return app
