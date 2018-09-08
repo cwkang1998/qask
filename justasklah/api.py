0b86be4a66d804d3cfc5c1ffba6247d572adf1a5
chenwenkang <cwk1998@hotmail.com>
2018-09-08 23:31:48 +0800

    add get endpoints for getting all users

--- justasklah/api.py
+++ justasklah/api.py
@@ -3,9 +3,21 @@ from flask.views import MethodView
 from flask_socketio import emit, Namespace
 
 from .db import mongo
+import bson
 
 api_bp = Blueprint('api', __name__, url_prefix='/')
 
+class UserView(MethodView):
+    def get(self):
+        cur = mongo.db.user.find({})
+        users = []
+        for doc in cur:
+            users.append(doc)
+        cur.close()
+        return jsonify(users)
+
+    def post(self):
+        pass
 
 class RoomView(MethodView):
 
@@ -15,24 +27,6 @@ class RoomView(MethodView):
     def post(self):
         pass
 
-
-class UserView(MethodView):
-    def get(self):
-        queryset = User.objects.all()
-        pass
-
-    def post(self):
-        pass
-
-
-class MessageView(MethodView):
-    def get(self):
-        pass
-
-    def post(self):
-        pass
-
-
 class MessageSocket(Namespace):
 
     def on_connect(self):
@@ -40,3 +34,15 @@ class MessageSocket(Namespace):
 
     def on_disconnect(self):
         pass
+
+    def on_message(self):
+        pass
+
+    def on_like(self):
+        pass
+    
+    def on_dismiss(self):
+        pass
+
+api_bp.add_url_rule('/user', view_func=UserView.as_view('user'))
+api_bp.add_url_rule('/room', view_func=RoomView.as_view('room'))
