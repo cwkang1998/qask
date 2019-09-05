b2a92e34002104a6adf6c12ad3dbe43b56b3d8d5
jhchee <cheehong05090@gmail.com>
2019-09-05 17:30:58 +0800

    condense docker fresh and normal script into one

--- docker-fresh-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf data
-mkdir -p data/{db,db_log}
-sudo chmod 777 data/*
-docker-compose -f docker-compose.yml up --force-recreate --build
--- docker-normal-run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf data
-mkdir -p data/{db,db_log}
-sudo chmod 777 data/*
-docker-compose -f docker-compose.yml build
-docker-compose -f docker-compose.yml up -d
