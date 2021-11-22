f5bb54e959fa8a1bfd8327a047a981ab03a92449
chen <23054115+cwkang1998@users.noreply.github.com>
2021-11-22 23:58:48 +0800

    feat: Add send button for the question box

--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -1,12 +1,35 @@
 import React from "react";
-import { Container, Stack, TextareaAutosize } from "@mui/material";
+import { Button, Container, Stack, TextareaAutosize } from "@mui/material";
 import { QuestionList } from "../components/QuestionList";
 
 export const Room = () => {
   return (
-    <Container>
+    <Container style={{ padding: "16px", height: "100%" }}>
       <Stack spacing={2}>
-        <TextareaAutosize minRows={5} placeholder="Ask your questions here!" />
+        <Stack
+          spacing={1}
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <TextareaAutosize
+            style={{
+              width: "100%",
+            }}
+            minRows={5}
+            placeholder="Ask your questions here!"
+          />
+          <Button
+            variant="contained"
+            size="large"
+            style={{
+              height: "100%",
+            }}
+          >
+            Send
+          </Button>
+        </Stack>
+
         <QuestionList />
       </Stack>
     </Container>
